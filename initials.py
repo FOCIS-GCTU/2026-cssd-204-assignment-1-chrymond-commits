7878832195ad037ec2ff69149ebbb23ae80e8c95
chrymond-commits <chrymond@gmail.com>
2026-05-31 18:23:59 +0000

    Assignment 1

--- initials.py
+++ initials.py
@@ -1,7 +1,39 @@
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Pearl Maame-Esi Dentsil
+# STUDENT ID: 2425401770
+# Email: chrymond@gmail.com
+# Grader: Augustus
+#
+# On my honor, Pearl Maame-Esi Dentsil, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # This program prints my initials in large stylized letters
 
+    print()
+    print("...PMED")
+    print()
 
+    print("...PPPPPPPPPPP......MMMM......MMMM......EEEEEEEEEEEE......DDDDDDDDDD....")
+    print("...PPPPPPPPPPP......MMMM......MMMM......EEEEEEEEEEEE......DDDDDDDDDDDD..")
+    print("...PPP.....PPP......MMM.MM..MM.MMM......EEE...............DDD.......DDD.")
+    print("...PPP.....PPP......MMMM..MMM..MMM......EEE...............DDD.......DDD.")
+    print("...PPPPPPPPPPP......MMM...MM...MMM......EEEEEEEEEEEE......DDD.......DDD.")
+    print("...PPPPPPPPPPP......MMM........MMM......EEEEEEEEEEEE......DDD.......DDD.")
+    print("...PPP..............MMM........MMM......EEE...............DDD.......DDD.")
+    print("...PPP..............MMM........MMM......EEE...............DDD.......DDD.")
+    print("...PPP..............MMM........MMM......EEEEEEEEEEEE......DDDDDDDDDDDD..")
+    print("...PPP..............MMM........MMM......EEEEEEEEEEEE......DDDDDDDDDD....")
+    print()
 
+ 
 main()
+
+
+
+
+
